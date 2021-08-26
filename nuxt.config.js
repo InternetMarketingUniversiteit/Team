be3dd0a0425709d26ccf90c2f82e018293bd97e0
Fenna Veenstra <fenna@phoenixsite.nl>
2021-08-26 16:41:45 +0200

    add header mobile viewport

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,16 @@ export default {
     ],
     components: true,
     head: {
+        title: 'my website title',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'my website description'
+            }
+        ],
         link: [
             {
                 rel: 'stylesheet',
