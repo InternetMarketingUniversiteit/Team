72f2047b4c18b565ca09a02c17f78a67d280cec8
Fenna Veenstra <fenna@phoenixsite.nl>
2021-08-26 15:14:22 +0200

    navigation

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,22 +1,21 @@
 <template>
-    <div>
+    <div class="d-flex">
         <v-bottom-navigation>
-            <v-btn value="recent">
-                <span>Recent</span>
-                <i class="fa fa-check"></i>
-                <v-icon>mdi-history</v-icon>
+            <v-btn value="overzicht" href="/dashboard">
+                <span>Overzicht</span>
+                <i class="fa fa-home"></i>
             </v-btn>
-
-            <v-btn value="favorites">
-                <span>Favorites</span>
-
-                <v-icon>mdi-heart</v-icon>
+            <v-btn value="games" href="/games">
+                <span>Games</span>
+                <i class="fa fa-gamepad"></i>
             </v-btn>
-
-            <v-btn value="nearby">
-                <span>Nearby</span>
-
-                <v-icon>mdi-map-marker</v-icon>
+            <v-btn value="agenda" href="/calendar">
+                <span>Agenda</span>
+                <i class="fa fa-calendar"></i>
+            </v-btn>
+            <v-btn value="profile" href="/profile">
+                <span>Profile</span>
+                <i class="fa fa-user"></i>
             </v-btn>
         </v-bottom-navigation>
     </div>
@@ -24,6 +23,6 @@
 <script>
 
 </script>
-<style>
+<style lang="scss">
 
 </style>
