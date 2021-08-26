a4dfdc2cd7bd21e1a3e8e4cfa8a359e2eb475e54
Fenna Veenstra <fenna@phoenixsite.nl>
2021-08-26 15:39:44 +0200

    navigation fix

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,28 +1,17 @@
 <template>
     <div class="d-flex">
-        <v-bottom-navigation>
-            <v-btn value="overzicht" href="/dashboard">
-                <span>Overzicht</span>
-                <i class="fa fa-home"></i>
-            </v-btn>
-            <v-btn value="games" href="/games">
-                <span>Games</span>
-                <i class="fa fa-gamepad"></i>
-            </v-btn>
-            <v-btn value="agenda" href="/calendar">
-                <span>Agenda</span>
-                <i class="fa fa-calendar"></i>
-            </v-btn>
-            <v-btn value="profile" href="/profile">
-                <span>Profile</span>
-                <i class="fa fa-user"></i>
-            </v-btn>
-        </v-bottom-navigation>
+        <NuxtLink to="/dashboard">
+            <span>Overzicht</span>
+        </NuxtLink>
+        <NuxtLink to="/games">
+            <span>Games</span>
+        </NuxtLink>
+        <NuxtLink to="/calendar">
+            <span>Agenda</span>
+        </NuxtLink>
+        <NuxtLink to="/profile">
+            <span>Profile</span>
+        </NuxtLink>
     </div>
 </template>
-<script>
 
-</script>
-<style lang="scss">
-
-</style>
