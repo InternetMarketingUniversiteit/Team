dca139fbad8dbb55843108fc224f2d5d382cb0a9
Reindert Vetter <reindert@plugandpay.nl>
2021-08-26 15:03:32 +0200

    index users

--- tests/Http/User/UserIndexTest.php
+++ tests/Http/User/UserIndexTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Http\User;
 
+use Database\Factories\UserFactory;
 use Tests\TestCase;
 
 class UserIndexTest extends TestCase
@@ -9,9 +10,14 @@ class UserIndexTest extends TestCase
     /** @test */
     public function basic_users(): void
     {
+        UserFactory::new()->create();
         $response = $this->get('/api/users');
 
         $response->assertStatus(200);
-        $response->assertJson([]);
+        $response->assertJsonPath('data.0', [
+            'name'          => 'Adah Reichel',
+            'email'         => 'wziemann@example.net',
+            'date_of_birth' => '2021-08-06',
+        ]);
     }
 }
